f2b5db7b094fdd3c076cadb6b8fd485f2d0731d6
Eric Kramer <cosmocracy@yahoo.com>
2017-01-30 18:52:44 -0500

    Queries data (MySQL) to populate a grid with a subset of fields; CRUD buttons configured for NO-OP (will remove them); Fields are sortable

--- src/main/java/org/nch/research/hgmd/ui/MainUI.java
+++ src/main/java/org/nch/research/hgmd/ui/MainUI.java
@@ -1,7 +1,140 @@
 package org.nch.research.hgmd.ui;
 
-/**
- * Created by Jannimer on 1/30/2017.
- */
-public class MainUI {
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import org.nch.research.hgmd.db.HGMD;
+import org.nch.research.hgmd.db.HgmdRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.vaadin.viritin.button.ConfirmButton;
+import org.vaadin.viritin.button.MButton;
+import org.vaadin.viritin.components.DisclosurePanel;
+import org.vaadin.viritin.fields.MTable;
+import org.vaadin.viritin.fields.MTextField;
+import org.vaadin.viritin.label.RichText;
+import org.vaadin.viritin.layouts.MHorizontalLayout;
+import org.vaadin.viritin.layouts.MVerticalLayout;
+
+@Title("HGMD Search")
+@Theme("valo")
+@SpringUI
+public class MainUI extends UI {
+
+    private static final long serialVersionUID = 1L;
+
+    HgmdRepository repo;
+
+    private MTable<HGMD> list = new MTable<>(HGMD.class)
+            .withProperties("entrezId", "accession", "classification", "omimId", "alternateSymbol")
+            // .withColumnHeaders("id", "Name", "Email")
+            .setSortableProperties("entrezId", "accession", "classification", "omimId", "alternateSymbol")
+            .withFullWidth();
+
+    private TextField filterByName = new MTextField()
+            .withInputPrompt("Filter by ID");
+    private Button addNew = new MButton(FontAwesome.PLUS, this::add);
+    private Button edit = new MButton(FontAwesome.PENCIL_SQUARE_O, this::edit);
+    private Button delete = new ConfirmButton(FontAwesome.TRASH_O,
+            "Are you sure you want to delete the entry?", this::remove);
+
+    public MainUI(HgmdRepository r) {
+        this.repo = r;
+    }
+
+    @Override
+    protected void init(VaadinRequest request) {
+        DisclosurePanel aboutBox = new DisclosurePanel("HGMD Search Utility", new RichText().withMarkDown("Hi!")); // .withMarkDownResource("/welcome.md"));
+        setContent(
+                new MVerticalLayout(
+                        aboutBox,
+                        new MHorizontalLayout(filterByName, addNew, edit, delete),
+                        list
+                ).expand(list)
+        );
+        listEntities();
+
+        list.addMValueChangeListener(e -> adjustActionButtonState());
+        filterByName.addTextChangeListener(e -> {
+            listEntities(e.getText());
+        });
+    }
+
+    protected void adjustActionButtonState() {
+        boolean hasSelection = list.getValue() != null;
+        edit.setEnabled(hasSelection);
+        delete.setEnabled(hasSelection);
+    }
+
+    static final int PAGESIZE = 45;
+
+    private void listEntities() {
+        listEntities(filterByName.getValue());
+    }
+
+    private void listEntities(String nameFilter) {
+        // A dead simple in memory listing would be:
+        // list.setRows(repo.findAll());
+
+        // But we want to support filtering, first add the % marks for SQL name query
+        String likeFilter = "%" + nameFilter + "%";
+        // list.setRows(repo.findByNameLikeIgnoreCase(likeFilter));
+        list.setRows(repo.findAll());
+
+        // Lazy binding for better optimized connection from the Vaadin Table to
+        // Spring Repository. This approach uses less memory and database
+        // resources. Use this approach if you expect you'll have lots of data
+        // in your table. There are simpler APIs if you don't need sorting.
+        // list.lazyLoadFrom(
+        //         // entity fetching strategy
+        //         (firstRow, asc, sortProperty) -> repo.findByNameLikeIgnoreCase(
+        //                 likeFilter,
+        //                 new PageRequest(
+        //                         firstRow / PAGESIZE,
+        //                         PAGESIZE,
+        //                         asc ? Sort.Direction.ASC : Sort.Direction.DESC,
+        //                         // fall back to id as "natural order"
+        //                         sortProperty == null ? "id" : sortProperty
+        //                 )
+        //         ),
+        //         // count fetching strategy
+        //         () -> (int) repo.countByNameLike(likeFilter),
+        //         PAGESIZE
+        // );
+        adjustActionButtonState();
+
+    }
+
+    public void add(ClickEvent clickEvent) {
+        // edit(new Person());
+    }
+
+    public void edit(ClickEvent e) {
+        // edit(list.getValue());
+    }
+
+    public void remove(ClickEvent e) {
+//        repo.delete(list.getValue());
+//        list.setValue(null);
+//        listEntities();
+    }
+
+    protected void edit(final HGMD record) {
+//        personForm.setEntity(phoneBookEntry);
+//        personForm.openInModalPopup();
+    }
+
+//    @EventBusListenerMethod(scope = EventScope.UI)
+//    public void onPersonModified(PersonModifiedEvent event) {
+//        listEntities();
+//        personForm.closePopup();
+//    }
+
 }
--- src/main/java/org/nch/research/hgmd/ui/SampleVaadinUI.java
+++ src/main/java/org/nch/research/hgmd/ui/SampleVaadinUI.java
@@ -7,7 +7,7 @@ import com.vaadin.ui.Button;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
 
-@SpringUI
+// @SpringUI
 @Theme("valo")
 public class SampleVaadinUI extends UI {
 
